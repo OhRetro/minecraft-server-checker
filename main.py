b538e00f8fa560b2baec5a4d9ffcd1d88a57ed7a
OhRetro <gabrielnacazone@hotmail.com>
2022-02-27 15:59:20 -0300

    2.1.2

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 #Minecraft Server Checker
-_version = "2.1.1"
+_version = "2.1.2"
 
 #Imports
 try:
-    from mcstatus import MinecraftServer
-    from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox
-    from PyQt5 import uic
     import webbrowser
+    from mcstatus import MinecraftServer
+    from oreto_utils import PyQt
+    from PyQt5 import uic
+    from PyQt5.QtWidgets import QApplication, QWidget
     
+    Icon = PyQt.Icon
+    Button = PyQt.Button
+
 except ImportError as missing_package:
     missing_package = str(missing_package).replace("No module named ", "")
     print(f"{missing_package} is missing.\nPlease install {missing_package}.\n")
     exit(0)
 
 finally:
-    from socket import gaierror as InvalidMinecraftServerIp, timeout as NoResponse
     import sys
+    from socket import gaierror as InvalidMinecraftServerIp
+    from socket import timeout as NoResponse
 
-#Minecraft server Checker
+#Minecraft Server Checker
 class MinecraftServerChecker(QWidget):
     def __init__(self):
         super().__init__()
@@ -29,80 +34,91 @@ class MinecraftServerChecker(QWidget):
         def check_server():
             try:
                 if server_ip := gui.ServerIP_display.text():
-                    set_gui_text("Refresh")
-                    
+                    PyQt.set_text(gui, Check_button="Refresh")
+
                     server = MinecraftServer.lookup(server_ip)
                     status = server.status()
-                    
-                    set_gui_text(check_button="Refresh",
-                                 status="Online",
-                                 players=f"{status.players.online}/{status.players.max}",
-                                 ping=f"{round(status.latency)} ms",
-                                 server_software=str(status.version.name),
-                                 server_motd=str(status.description)
-                                 )
+
+                    PyQt.set_text(
+                        gui,
+                        Check_button="Refresh",
+                        Status_display="Online",
+                        Players_display=f"{status.players.online}/{status.players.max}",
+                        Ping_display=f"{round(status.latency)} ms",
+                        ServerSoftware_display=str(status.version.name),
+                        ServerMOTD_display=str(status.description))
 
                 else:
                     reset()
-                    display_message("Server IP field Empty", "The Server IP field is empty")
+                    PyQt.display_message(
+                        title="Server IP field Empty", 
+                        message="The Server IP field is empty",
+                        icon=Icon.Information)
 
             #No Response
             except NoResponse:
                 reset()
-                set_gui_text("Refresh", "Offline")
+                PyQt.set_text(
+                    gui, 
+                    Check_button="Refresh",
+                    Status_display="Offline")
 
             #Invalid IP Address
             except InvalidMinecraftServerIp:
                 reset()
-                display_message("Invalid Minecraft Server IP", "The Server IP wasn't a Minecraft Server IP or was Invalid.")
+                PyQt.display_message(
+                    title="Invalid Minecraft Server IP",
+                    message="The Server IP wasn't a Minecraft Server IP or was Invalid.",
+                    icon=Icon.Information)
 
             #Connection Refused
             except ConnectionRefusedError:
                 reset()
-                display_message("Connection Refused", "The connection have been refused by the server.")
-                
+                PyQt.display_message(
+                    title="Connection Refused",
+                    message="The connection have been refused by the server.",
+                    icon=Icon.Information)
+
+            #Others
+            except Exception as error:
+                reset()
+                PyQt.display_message("Error", "An error has occurred", detailed_message=str(error), icon=Icon.Warning)
+
         #Reset
         def reset():
-            set_gui_text("Check")
-            
-        #Display Message
-        def display_message(title:str, message:str, standard_buttons=None):
-            display = QMessageBox()
-            display.setWindowTitle(title)
-            display.setText(message)
-
-            if standard_buttons != None:
-                display.setStandardButtons(standard_buttons)
-                return display.exec_()
-            else:   
-                display.exec_()
-                return
-
-        #Set GUI Text
-        def set_gui_text(check_button="", status="", players="", ping="", server_software="", server_motd=""):
-            gui.Check_button.setText(check_button)
-            gui.Status_display.setText(status)
-            gui.Players_display.setText(players)
-            gui.Ping_display.setText(ping)
-            gui.ServerSoftware_display.setText(server_software)
-            gui.ServerMOTD_display.setText(server_motd)
-
-        #About
-        def about():
-            chosen_response = display_message("Created by OhRetro", 
-                                              f"Minecraft Version Checker v{_version}\nDo you want to open the program's repository on github?", 
-                                              QMessageBox.Yes | QMessageBox.No)
-
-            if chosen_response == QMessageBox.Yes:
-                webbrowser.open("https://github.com/OhRetro/Minecraft-Server-Checker")
+            PyQt.set_text(
+                gui,
+                Check_button="Check",
+                Status_display="",
+                Players_display="",
+                Ping_display="",
+                ServerSoftware_display="",
+                ServerMOTD_display="")
 
         gui.Check_button.clicked.connect(check_server)
-        gui.MinecraftLogo_button.clicked.connect(about)
+        gui.MinecraftLogo_button.clicked.connect(self.about)
 
         gui.show()
 
+    #About
+    def about(self):
+        chosen_response = PyQt.display_message(
+            title="Created by OhRetro",
+            message=f"Minecraft Version Checker v{_version}\nDo you want to open the program's repository on github?",
+            icon=Icon.Question,
+            buttons= (Button.Yes | Button.No))
+
+        if chosen_response == Button.Yes:
+            webbrowser.open("https://github.com/OhRetro/Minecraft-Server-Checker")
+
 #Run
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MinecraftServerChecker()
-    app.exec()
+    try:
+        app = QApplication(sys.argv)
+        window = MinecraftServerChecker() 
+        
+    except Exception as error:
+        PyQt.display_message("Error", "An error has occurred", "Something went wrong", str(error), Icon.Critical)
+
+    finally:
+        app.exec()
